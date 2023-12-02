1a8f6d7e9c7edd7df14da3a67347033ac0c88290
Sean Kelliher <sbkelliher@icloud.com>
2023-12-02 11:17:12 -0500

    Add imports. All components now pulled directly into App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
-import ComponentHome from "./components/ComponentHome.vue";
+import Step1Introduction from "./components/Step1Introduction.vue";
+import Step2Longitude from "./components/Step2Longitude.vue";
+import Step3Latitude from "./components/Step3Latitude.vue";
+import Step4Percision from "./components/Step4Percision.vue";
+import Step5Review from "./components/Step5Review.vue";
+import Step6Result from "./components/Step6Result.vue";
 </script>
 
 <template>
-    <ComponentHome />
+    <div id="container">
+        <Step1Introduction />
+        <Step2Longitude />
+        <Step3Latitude />
+        <Step4Percision />
+        <Step5Review />
+        <Step6Result />
+    </div>
 </template>
 
 <style scoped>
