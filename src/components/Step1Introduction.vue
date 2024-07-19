ae4f32ae8cff7a48e441839d88e004e4eb8f9194
Sean Kelliher <sbkelliher@icloud.com>
2024-07-19 19:15:58 -0400

    Add preset locations. Some text changes too.

--- src/components/Step1Introduction.vue
+++ src/components/Step1Introduction.vue
@@ -5,12 +5,54 @@ import { store } from "../composables/store.js";
 <template>
     <main v-if="store.step === 'step1'">
         <section>
-            <h1>Let's get started.</h1>
+            <h1>Let's get started</h1>
         </section>
 
         <div class="results">
-            <p>We are going to convert longitude and latitude coordinates in the Degress, Minutes, Seconds format to the Digital Degrees format to use in computer-based mapping.</p>
+            <p>We'll convert longitude and latitude coordinates from the Degrees, Minutes, Seconds (DMS) format to the Digital Degrees (DD) format. DD format is used in computer-based mapping such as Mapbox, CARTO, and Google Maps. <strong>Don't have a location in mind?</strong> Use a preset location to see how this works.</p>
         </div>
+
+        <form>
+            <fieldset>
+                <legend>Locations</legend>
+                <input
+                    type="radio"
+                    id="sample-own"
+                    name="sample-locs"
+                    value="own"
+                    @input="store.updateSampleLocs($event.target.value)"
+                    :checked="store.sampleLocs === 'own'"
+                >
+                <label for="sample-own">Enter your own</label>
+                <input
+                    type="radio"
+                    id="eiffel-tower"
+                    name="sample-locs"
+                    value="eiffel"
+                    @input="store.updateSampleLocs($event.target.value)"
+                    :checked="store.sampleLocs === 'eiffel'"
+                >
+                <label for="eiffel-tower">Eiffel Tower, Paris</label>
+                <input
+                    type="radio"
+                    id="moma-museum"
+                    name="sample-locs"
+                    value="moma"
+                    @input="store.updateSampleLocs($event.target.value)"
+                    :checked="store.sampleLocs === 'moma'"
+                >
+                <label for="met-museum">Museum Modern Art, New York</label>
+                <input
+                    type="radio"
+                    id="hallgrim-church"
+                    name="sample-locs"
+                    value="hallgrim"
+                    @input="store.updateSampleLocs($event.target.value)"
+                    :checked="store.sampleLocs === 'hallgrim'"
+                >
+                <label for="hallgrim">Hallgrimskirkja, Reykjavík</label>
+            </fieldset>
+        </form>
 
         <nav>
             <button
