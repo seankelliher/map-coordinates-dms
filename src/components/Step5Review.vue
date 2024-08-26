5a3ab6058cb72e4d8d826197a1c38fb6129ef341
Sean Kelliher <sbkelliher@icloud.com>
2024-08-26 00:49:20 -0400

    Add computed properties, makes hemisphere like West title case.

--- src/components/Step5Review.vue
+++ src/components/Step5Review.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { computed } from "vue";
 import { store } from "../composables/store.js";
+
+// Makes longitude hemisphere, East or West, title case.
+const lonHemsTitleCase = computed(() => {
+    return store.lonHems.replace(/^./, store.lonHems[0].toUpperCase());
+});
+
+// Makes latitude hemisphere, North South, title case.
+const latHemsTitleCase = computed(() => {
+    return store.latHems.replace(/^./, store.latHems[0].toUpperCase());
+});
 </script>
 
 <template>
@@ -11,11 +22,11 @@ import { store } from "../composables/store.js";
         <div class="results">
             <p><strong>You entered these coordinates. Does everything look okay?</strong></p>
             <ul>
-                <li>Longitude: {{ store.lonDegs }} degs, {{ store.lonMins }} mins, {{ store.lonSecs }} secs, {{ store.lonHems }}</li>
+                <li>Longitude: {{ store.lonDegs }} degs, {{ store.lonMins }} mins, {{ store.lonSecs }} secs, {{ lonHemsTitleCase }}</li>
             </ul>
 
             <ul>
-                <li>Latitude: {{ store.latDegs }} degs, {{ store.latMins }} mins, {{ store.latSecs }} secs, {{ store.latHems }}</li>
+                <li>Latitude: {{ store.latDegs }} degs, {{ store.latMins }} mins, {{ store.latSecs }} secs, {{ latHemsTitleCase }}</li>
             </ul>                
 
             <ul>
