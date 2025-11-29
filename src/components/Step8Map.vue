0111117cfe3a6e1141ada1978c717bc7152f509e
Sean Kelliher <sbkelliher@icloud.com>
2025-11-28 23:26:45 -0500

    Add Step8, view full page map.

---
+++ src/components/Step8Map.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { store } from "../composables/store.js";
+import AreaMap from "./AreaMap.vue";
+</script>
+
+<template>
+    <div id="container" v-if="store.step === 'step8'">
+        <div id="map-area">
+            <AreaMap />
+        </div>
+
+        <div id="map-coords">
+            <dl>
+                <dt>Coordinates</dt>
+                <dd>Longitude: {{ store.resultLon }}</dd>
+                <dd>Latitude: {{ store.resultLat }}</dd>      
+            </dl>
+        </div>
+
+        <button
+            id="map-start-over"
+            tabindex="0"
+            @click="store.updateStep('step1'), store.resetVars()"
+            @keyup.enter="store.updateStep('step1'), store.resetVars()"
+        >
+            Start over
+        </button>
+    </div>
+</template>
+
+<style scoped>
+</style>
